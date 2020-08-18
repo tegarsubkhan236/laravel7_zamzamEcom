e6be779855a845e5e685f431090214ad60579dfc
tegarsubkhan236 <tegarsubkhan236@gmail.com>
2020-08-18 18:56:01 +0700

    secure_asset

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -28,6 +28,9 @@
     * License: https://bootstrapmade.com/license/
     ======================================================== -->
 </head>
+<!-- BOOTSTRAP -->
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"> </script>
 
 <body>
 
